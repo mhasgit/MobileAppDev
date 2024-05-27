616cb5da732f889139f0800ede76ae73be7dc732
mhasgit <91213473+mhasgit@users.noreply.github.com>
2024-05-27 06:55:19 +0500

    flutter practice

---
+++ personalpratice/personalpractice/android/app/src/main/kotlin/com/example/personalpractice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.personalpractice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ personalpratice/personalpractice/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Mobile App Development'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) =>
+              GestureDetector(
+                  onTap: () => pressed(index),
+                  child: Container(width: 50, height: 50, color: Colors.green, child: Text("Hello $index"))),
+        )
+    );
+  }
+
+  void pressed(int index) {
+    print (index);
+  }
+
+}
